3d31c7aaf0a43964d813058d8c135f6f4a9a97d7
Dylan Zhao <dylan.zhao@epita.fr>
2019-07-23 17:42:55 +0200

    adding html from totonio

--- application/djangoapp/urls.py
+++ application/djangoapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+app_name = 'business-intelligence'
+
 urlpatterns = [
     path('', views.index, name='index'),
 
@@ -9,6 +12,11 @@ urlpatterns = [
 
     path('test/', views.test, name='test'),
     path('hello/', views.hello, name='hello'),
+    path('info/', views.info, name='info'),
+    path('button/', views.button, name='button'),
+    path('request/', views.request, name='request'),
+    path('list/', views.list, name='list'),
+    path('add-article/', views.add_article, name='add-article'),
 
     # ------------------------------------
 
--- application/djangoapp/views.py
+++ application/djangoapp/views.py
@@ -1,21 +1,77 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from apipkg import api_manager as api
 
+# new import -------------------
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from .models import *
 
-def index(request):
-    time = api.send_request('scheduler', 'clock/time')
-    return HttpResponse("L'heure de la clock est %r" % time)
+from .forms import *
+
+
+# -----------------------------
+
+
+# def index(request):
+#   time = api.send_request('scheduler', 'clock/time')
+#   return HttpResponse("L'heure de la clock est %r" % time)
+
 
 # Changement par dylan --------------
 
 def test(request):
     return HttpResponse("Le test fonctionne en local")
 
+
 def hello(request):
     infoB = api.send_request('gestion-stock', 'info')
     return HttpResponse("Je suis BI et tu est : %r" % infoB)
 
-def info():
-    return HttpResponse("Je suis BI.")
+
+def info(request):
+    return HttpResponse("Je suis BI.r")
+
+
+# totonio- ---- - --  -- - - --
+
+def index(request):
+    info = api.send_request('gestion-stock', 'info')
+    return render(request, "index.html", {'info': info})
+
+
+def request(request):
+    text = api.send_request('gestion-stock', 'info')
+    return HttpResponse(text)
+
+
+def button(request):
+    context = {}
+    return render(request, "button.html", context)
+
+
+
+def add_article(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            first = Article.objects.all().filter(nom=form['nom'].value()).first()
+            if (first is None):
+                new_article = form.save()
+            else:
+                first.stock += int(form['stock'].value())
+                first.save()
+            return HttpResponseRedirect('/')
+    else:
+        form = ArticleForm()
+    return render(request, 'add_article.html', {'form': form})
+
+
+def list(request):
+    datas = Article.objects.all()
+    context = {
+        'articles': datas,
+    }
+    return render(request, "data.html", context)
 
 # ------------------------------------
