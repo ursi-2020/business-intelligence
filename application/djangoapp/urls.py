59afbbdab0c1ebbfe0cc1c286cb158cb5ae71b10
François KREMER <krmr.francois@gmail.com>
2019-09-30 17:33:53 +0200

    [MISC] Adding scheduler.

--- application/djangoapp/urls.py
+++ application/djangoapp/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('get_catalogue', views.get_catalogue, name='get_catalogue'),
     path('get_crm', views.get_crm, name='get_crm'),
     path('delete_catalogue_produit', views.delete_catalogue_produit, name='delete_catalogue_produit'),
-    path('delete_crm', views.delete_crm, name='delete_crm')
+    path('delete_crm', views.delete_crm, name='delete_crm'),
+    path('scheduler_crm', views.scheduler_crm, name='scheduler_crm'),
+    path('scheduler_catalogue_produit', views.scheduler_catalogue_produit, name='scheduler_catalogue_produit')
 ]
--- application/djangoapp/views.py
+++ application/djangoapp/views.py
@@ -6,6 +6,12 @@ from django.http import JsonResponse
 from django.core import serializers
 from .models import *
 
+from datetime import datetime, timedelta
+
+import requests
+
+from django.views.decorators.csrf import csrf_exempt
+
 from .forms import *
 
 import json
@@ -21,7 +27,7 @@ def crm(request):
     customers = Customer.objects.all()
     return render(request, "crm.html", {'customers': customers})    
 
-
+@csrf_exempt
 def get_catalogue(request):
     catalogue_request = api.send_request('catalogue-produit', 'api/data')
     json_data = json.loads(catalogue_request)
@@ -31,6 +37,7 @@ def get_catalogue(request):
             new_product.save()
     return catalogue_produit(request)
 
+@csrf_exempt
 def get_crm(request):
     crm_request = api.send_request('crm', 'api/data')
     print(crm_request)
@@ -49,4 +56,27 @@ def delete_catalogue_produit(request):
 def delete_crm(request):
     Customer.objects.all()
     return index(request)
+
+def scheduler_catalogue_produit(request):
+    clock_time = api.send_request('scheduler', 'clock/time')
+    time = datetime.strptime(clock_time, '"%d/%m/%Y-%H:%M:%S"')
+    time = time + timedelta(seconds=10)
+    schedule_task('business-intelligence','get_catalogue', time, 'minute', '{}', 'business-intelligence','automatic_fetch_catalogue_produit_db')
+    return catalogue_produit(request)
+
+def scheduler_crm(request):
+    clock_time = api.send_request('scheduler', 'clock/time')
+    time = datetime.strptime(clock_time, '"%d/%m/%Y-%H:%M:%S"')
+    time = time + timedelta(seconds=10)
+    schedule_task('business-intelligence','get_crm', time, 'minute', '{}', 'business-intelligence','automatic_fetch_crm_db')
+    return crm(request)
+
+def schedule_task(host, url, time, recurrence, data, source, name):
+    time_str = time.strftime('%d/%m/%Y-%H:%M:%S')
+    headers = {'Host': 'scheduler'}
+    data = {"target_url": url, "target_app": host, "time": time_str, "recurrence": recurrence, "data": data, "source_app": source, "name": name}
+    r = requests.post(api.api_services_url + 'schedule/add', headers = headers, json = data)
+    print(r.status_code)
+    print(r.text)
+    return r.text
 
