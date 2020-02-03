923ce7e5dccf3e9291a77fda1368e1dc4479295f
François KREMER <krmr.francois@gmail.com>
2020-02-03 16:13:26 +0100

    Correction du probleme de conflit avec les migrations.

--- application/djangoapp/migrations/0013_incident.py
+++ application/djangoapp/migrations/0013_incident.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djangoapp', '0011_chiffre_affaire_benefice'),
+        ('djangoapp', '0012_ajout_origine'),
     ]
 
     operations = [
--- application/djangoapp/migrations/0014_auto_20200203_1022.py
+++ application/djangoapp/migrations/0014_auto_20200203_1022.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djangoapp', '0012_ajout_origine'),
+        ('djangoapp', '0013_incident'),
     ]
 
     operations = [
