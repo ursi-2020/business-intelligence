58c410f962379241ed25550e51f36a3dc7fe3ff7
François KREMER <krmr.francois@gmail.com>
2020-02-03 22:49:39 +0100

    Préparation pour les factures et correction de CRM.

--- application/djangoapp/api/gesco.py
+++ application/djangoapp/api/gesco.py
@@ -14,16 +14,15 @@ from django.views.decorators.csrf import csrf_exempt
 myappurl = "http://localhost:" + os.environ["WEBSERVER_PORT"]
 
 def get_bill(jsonLoad):
-    return True
-    #if not isinstance(jsonLoad["body"], dict):
-    #    delivery = json.loads(jsonLoad["body"].replace("\'", "\""))
-    #else:
-    #    delivery = jsonLoad["body"]
-    #print(jsonLoad)
-    #deliv = Delivery(type=type, idCommande=delivery["idCommande"])
-    #deliv.save()
-    #for produit in delivery['produits']:
-    #    new_produit = DeliveredProduct(codeProduit=produit['codeProduit'],
-    #                                   quantite=produit['quantite'],
-    #                                   delivery=deliv)
-    #    new_produit.save()
+    if not isinstance(jsonLoad["body"], dict):
+        delivery = json.loads(jsonLoad["body"].replace("\'", "\""))
+    else:
+        delivery = jsonLoad["body"]
+    print(jsonLoad)
+    deliv = Delivery(type=type, idCommande=delivery["idCommande"])
+    deliv.save()
+    for produit in delivery['produits']:
+        new_produit = DeliveredProduct(codeProduit=produit['codeProduit'],
+                                       quantite=produit['quantite'],
+                                       delivery=deliv)
+        new_produit.save()
