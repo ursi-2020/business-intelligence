49086ba9b7068266c894741c4be9a3c3606f8c89
François KREMER <krmr.francois@gmail.com>
2020-02-04 17:47:58 +0100

    On revert.

--- application/djangoapp/views.py
+++ application/djangoapp/views.py
@@ -70,17 +70,10 @@ def stock(request):
     #now = datetime.strptime(clock_time, '"%d/%m/%Y-%H:%M:%S"')
     #stocks = Stock.objects.filter(date__year=now.year, date__month=now.month, date__day=now.day)
     stocks_list = Stock.objects.all().order_by('-date')
-    val_stock = 0
-    if stocks_list:
-        date = stocks_list[0].date
-        daily_stocks = Stock.objects.filter(date__gte=date)
-        for stock in daily_stocks:
-            produit = Produit.objects.get(codeProduit=stock.codeProduit)
-            val_stock = val_stock + (produit.prix * stock.quantite)
     paginator = Paginator(stocks_list, 10) # Show 25 contacts per page
     page = request.GET.get('page')
     stocks = paginator.get_page(page)
-    return render(request, "stock.html", {'stocks': stocks, "val_stock": round(val_stock/100, 2)})
+    return render(request, "stock.html", {'stocks': stocks})
 
 
 def stock_magasin(request):
@@ -88,17 +81,10 @@ def stock_magasin(request):
     #now = datetime.strptime(clock_time, '"%d/%m/%Y-%H:%M:%S"')
     #stocks = StockMagasin.objects.filter(date__year=now.year, date__month=now.month, date__day=now.day)
     stocks_list = StockMagasin.objects.all().order_by('-date')
-    val_stock = 0
-    if stocks_list:
-        date = stocks_list[0].date
-        daily_stocks = StockMagasin.objects.filter(date__gte=date)
-        for stock in daily_stocks:
-            produit = Produit.objects.get(codeProduit=stock.codeProduit)
-            val_stock = val_stock + (produit.prix * stock.stockDisponible)
     paginator = Paginator(stocks_list, 10) # Show 25 contacts per page
     page = request.GET.get('page')
     stocks = paginator.get_page(page)
-    return render(request, "stock_magasin.html", {'stocks': stocks, "val_stock": round(val_stock/100, 2)})
+    return render(request, "stock_magasin.html", {'stocks': stocks})
 
 def deliveries(request):
     deliveries_list = Delivery.objects.all()
